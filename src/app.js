504934a8398b300339a167a49bfb0b5d83296640
Karan Shah <karanshah0206@outlook.com>
2020-06-08 11:32:11 +0530

    SLL Certificate

--- src/app.js
+++ src/app.js
@@ -21,13 +21,16 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 var stream = require('./ws/stream');
 var path = require('path');
-const port = 3000;
+const port = 3100;
 
 // Passing UI
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/pages', express.static(path.join(__dirname, 'pages')));
 app.use('/index.html', express.static(path.join(__dirname + '/index.html')));
 app.get('/', (req, res)=>{res.sendFile(__dirname+'/index.html');});
+
+app.use('/.well-known/pki-validation/BEDCF239D9946A175A3E89A67CE52CDA.txt', express.static(path.join(__dirname + '/.well-known/pki-validation/BEDCF239D9946A175A3E89A67CE52CDA.txt')));
+app.get('/.well-known/pki-validation', (req, res)=>{res.sendFile(__dirname+'/.well-known/pki-validation/BEDCF239D9946A175A3E89A67CE52CDA.txt');});
 
 // Establish Connection
 try {
