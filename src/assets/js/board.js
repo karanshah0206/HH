a20a116341f7d0572cc3ede02e1944c955f9e126
Karan Shah <karanshah0206@outlook.com>
2020-06-24 16:47:39 +0530

    Clear Board

--- src/assets/js/board.js
+++ src/assets/js/board.js
@@ -1,2 +1,6 @@
 // Fabric
 var canvas = new fabric.Canvas('board-child', {isDrawingMode: true});
+
+document.getElementById('clear-board-btn').addEventListener('click', () => {
+    canvas.clear();
+})
