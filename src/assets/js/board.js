fb1807474fb0d158f9d65f5dc1ae695d4f2e30aa
Karan Shah <karanshah0206@outlook.com>
2020-06-24 16:59:23 +0530

    Added Functionality To Download Button

--- src/assets/js/board.js
+++ src/assets/js/board.js
@@ -1,6 +1,17 @@
 // Fabric
 var canvas = new fabric.Canvas('board-child', {isDrawingMode: true});
 
+// Clear Canvas
 document.getElementById('clear-board-btn').addEventListener('click', () => {
     canvas.clear();
+})
+
+// Download Canvas
+document.getElementById('download-board-btn').addEventListener('click', () => {
+    var link = document.createElement('a');
+    link.id = 'downloadTemp';
+    link.download = 'Board.png';
+    link.href = document.getElementById('board-child').toDataURL();
+    link.click();
+    $('#downloadTemp').remove();
 })
