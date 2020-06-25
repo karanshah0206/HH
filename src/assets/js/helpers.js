eeaa876132f4541118b7a74e8dd246b6ba03c547
Karan Shah <karanshah0206@outlook.com>
2020-06-25 11:28:11 +0530

    Removed Call To Ice Server

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -102,25 +102,6 @@ export default {
             throw new Error('User media not available');
         }
     },
-
-    // Ice Server
-    getIceServer(){
-        return {
-            iceServers: [
-                {
-                    urls: ["stun:eu-turn4.xirsys.com"]
-                }, 
-                {
-                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
-                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
-                    urls: [
-                        "turn:eu-turn4.xirsys.com:80?transport=udp",
-                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
-                    ]
-                }
-            ]
-        };
-    },
     
     addChat(data, senderType){
         let chatMsgDiv = document.querySelector('#chat-messages');
