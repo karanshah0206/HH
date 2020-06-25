f16a89d9da44fb9f3e8389cfaf496ac3309fab5f
Karan Shah <karanshah0206@outlook.com>
2020-06-25 10:59:04 +0530

    Changed Zoom Message Styling

--- src/assets/js/board.js
+++ src/assets/js/board.js
@@ -27,11 +27,7 @@ document.getElementById('zoom-in-board-btn').addEventListener('click', () => {
         currentZoom *= 2;
     }
     canvas.setZoom(currentZoom);
-    if (currentZoom!= 1) {
-        document.getElementById('zoom-info').innerText = 'Zoom: ' + currentZoom + 'x';
-    } else {
-        document.getElementById('zoom-info').innerText = '';
-    }
+    document.getElementById('zoom-info').innerText = + currentZoom + 'x ';
     canvas.setWidth(1500 * canvas.getZoom());
     canvas.setHeight(3000 * canvas.getZoom());
 })
@@ -44,11 +40,7 @@ document.getElementById('zoom-out-board-btn').addEventListener('click', () => {
         currentZoom /= 2;
     }
     canvas.setZoom(currentZoom);
-    if (currentZoom!= 1) {
-        document.getElementById('zoom-info').innerText = 'Zoom: ' + currentZoom + 'x';
-    } else {
-        document.getElementById('zoom-info').innerText = '';
-    }
+    document.getElementById('zoom-info').innerText = + currentZoom + 'x ';
     canvas.setWidth(1500 * canvas.getZoom());
     canvas.setHeight(3000 * canvas.getZoom());
 })
