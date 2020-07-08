5c3f6b88d3ccb8cbdea6dce56b131d7586886a2f
Karan Shah <karanshah0206@outlook.com>
2020-07-08 23:24:45 +0530

    Modified ICE server

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -106,19 +106,7 @@ export default {
     // Ice Server
     getIceServer(){
         return {
-            iceServers: [
-                {
-                    urls: ["stun:eu-turn4.xirsys.com"]
-                }, 
-                {
-                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
-                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
-                    urls: [
-                        "turn:eu-turn4.xirsys.com:80?transport=udp",
-                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
-                    ]
-                }
-            ]
+            iceServers: [{urls: ["stun:stun.l.google.com:19302"]}]
         };
     },
     
